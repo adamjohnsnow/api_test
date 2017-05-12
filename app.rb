6fc5f88db7984c6a280f33b8e09d3a361ea410c8
Adam Snow <adamjohnsnow@icloud.com>
2017-05-12 15:55:16 +0100

    add headers

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'json'
 
 class ThermostatApi < Sinatra::Base
   get '/thermostat' do
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     content_type :json
     File.read('views/thermostat.json')
   end
