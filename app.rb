5061fdf2c6139a7b1e7ef73fe403e03fc2f836ec
Adam Snow <adamjohnsnow@icloud.com>
2017-05-12 18:14:44 +0100

    change json

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'json'
 class ThermostatApi < Sinatra::Base
   get '/thermostat' do
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     content_type :json
