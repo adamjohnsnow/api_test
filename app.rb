e8fbaaf5711ee491fd8b7b79a9b2124acc393a01
Adam Snow <adamjohnsnow@icloud.com>
2017-05-14 15:27:38 +0100

    post path access rights

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'json'
 class ThermostatApi < Sinatra::Base
   get '/thermostat' do
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'GET'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     content_type :json
@@ -12,6 +12,9 @@ class ThermostatApi < Sinatra::Base
   end
 
   post '/save_thermostat' do
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST'
+
     jsonHash = {
       "temperature" => params["temperature"].to_i,
       "powerSave" => is_true?(params["powerSave"])
