053c677b0263ad02c4bfed793649cf68f74da4d4
Adam Snow <adamjohnsnow@icloud.com>
2017-06-26 11:07:10 +0100

    add captcha route

--- app.rb
+++ app.rb
@@ -11,10 +11,18 @@ class ThermostatApi < Sinatra::Base
     File.read('views/thermostat.json')
   end
 
+  get '/captcha' do
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    content_type :json
+    File.read('views/captcha.json')
+  end
+
   post '/save_thermostat' do
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST'
-
     jsonHash = {
       "temperature" => params["temperature"].to_i,
       "powerSave" => is_true?(params["powerSave"])
